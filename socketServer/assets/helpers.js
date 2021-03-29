84bae181265799759bc2a04de0de13f89b002fc3
aaron-wc <likeclem30@gmail.com>
2021-03-29 11:54:10 -0500

    Added db connection

--- socketServer/assets/helpers.js
+++ socketServer/assets/helpers.js
@@ -2,11 +2,9 @@ const socketUser = require("./socketUser");
 
 const helpers = {};
 
-helpers.zenoTripDBLocal =
-  "mongodb://lagride:lagride@172.30.218.218:27017/lagride";
-
-helpers.zenoTripDBLIve =
-  "mongodb://lagride:lagride@172.30.218.218:27017/lagride";
+helpers.zenoTripDBLocal = "mongodb://localhost:27017/zeno_trip";
+helpers.zenoTripDBLive =
+  "mongodb://lagridemdb:lagridemdb@172.30.218.218:27017/lagridemdb";
 // helpers.zenoTripDBLIve = 'mongodb+srv://increase_21:QRudhu0Fsw0b166S@cluster0-mszft.mongodb.net/zeno_trip?retryWrites=true&w=majority';
 
 // for generating token
